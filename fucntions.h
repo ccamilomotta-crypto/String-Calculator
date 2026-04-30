d7775a06f0208209bfeab23be9eabc5ec2a52f4d
ccamilomotta-crypto <ccamilomotta@gmail.com>
2026-04-29 23:37:44 -0500

    Add function declarations for basic arithmetic operations

---
+++ fucntions.h
@@ -0,0 +1,36 @@
+// this file is provided.  do not modify. do not submit.
+#ifndef PROVIDED_FUNCTIONS_H_
+#define PROVIDED_FUNCTIONS_H_
+
+#include <string>
+
+using std::string;
+
+/**
+ * Add two numbers.
+ *
+ * @param lhs lefthand side
+ * @param rhs righthand side
+ * @returns the sum of `lhs` plus `rhs`
+ */
+string add(const string& lhs, const string& rhs);
+
+/**
+ * Subtract two numbers.
+ *
+ * @param lhs lefthand side
+ * @param rhs righthand side
+ * @returns the difference of `lhs` minus `rhs`
+ */
+string subtract(const string& lhs, const string& rhs);
+
+/**
+ * Multiply two numbers.
+ *
+ * @param lhs lefthand side
+ * @param rhs righthand side
+ * @returns the product of `lhs` times `rhs`
+ */
+string multiply(const string& lhs, const string& rhs);
+
+#endif  // PROVIDED_FUNCTIONS_H_
